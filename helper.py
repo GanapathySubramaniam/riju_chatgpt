62a268de42f999638636c9a572e885cba8fbec8d
GanapathySubramaniam <105225199+GanapathySubramaniam@users.noreply.github.com>
2023-04-05 21:23:30 +0530

    Update helper.py

--- helper.py
+++ helper.py
@@ -4,9 +4,10 @@ import streamlit as st
 import requests
 import openai
 
+
+openai.api_key =read_yaml('secrets.yaml')['api_key']
+
 def generate(content,messages):
-    api_key=read_yaml('secrets.yaml')['api_key']
-    openai.api_key =api_key
     messages.append({"role": "user", "content": content})
     completion = openai.ChatCompletion.create(
     model="gpt-3.5-turbo",
